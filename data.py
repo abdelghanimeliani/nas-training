6480bdef67e99baf379f0e3078ba21e5e72b2c43
abdelghanimeliani <a.meliani@esi-sba.dz>
2025-11-07 10:41:42 +0100

    add new expiriment dataset: alibaba 2017 traces

--- data.py
+++ data.py
@@ -9,10 +9,12 @@ import pandas as pd
 
 df1 = pd.read_csv("./data/timetrack/compute_dataset.csv")
 df2 = pd.read_csv("./data/materna/15.csv")
+df3 = pd.read_csv("./data/alibaba/data.csv")
 
 # Take the first 8000 values from the chosen columns
-ts1 = df1['averageCpuUsagePerse'].iloc[:8000].astype(float)
-ts2 = df2['CPU usage [%]'].iloc[:8000].str.replace(',', '.').astype(float)
+ts1 = df1['averageCpuUsagePerse'].iloc[:140].astype(float)
+ts2 = df2['CPU usage [%]'].iloc[:140].str.replace(',', '.').astype(float)
+ts3 = df3['util:CPU'].iloc[:140].astype(float)
 
 # Min-max normalization function
 def min_max_norm(series):
@@ -20,7 +22,9 @@ def min_max_norm(series):
 
 ts1_norm = min_max_norm(ts1)
 ts2_norm = min_max_norm(ts2)
+ts3_norm = min_max_norm(ts3)
 
 # Save normalized data
-ts1_norm.to_csv("./dataset1_8000.csv", index=False)
-ts2_norm.to_csv("./dataset2_8000.csv", index=False)
+ts1_norm.to_csv("./dataset1_140.csv", index=False)
+ts2_norm.to_csv("./dataset2_140.csv", index=False)
+ts3_norm.to_csv("./dataset3_140.csv", index=False)
